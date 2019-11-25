bb8a6522351dc8dd61e5efb77d1e27e928b141c0
Jensen <jen@Jensens-MacBook-Pro.local>
2019-11-25 14:54:06 +0800

    sim button

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import plotly.express as px
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
+k = 0
 persons, houses, ask_df, bid_df = initialize()
 heatmap_data = [[
     round(houses.iloc[i + j * 10]['last_updated'], 2) for i in range(10)
@@ -26,24 +27,22 @@ heatmap_data = [[
 # heatmap_data=[list(df.iloc[i]) for i in range(len(df))][::-1]
 
 content = [
+    dbc.Button("Simulate!", id="start-button", className="mr-2"),
+    html.Span(id="sim-count", style={"vertical-align": "middle"}),
     dcc.Graph(
         id='live-update-graph',
         figure=go.Figure(
             data=go.Heatmap(
                 z=heatmap_data,
                 y=[i for i in range(10)],
-                x=[i for i in range(10)],
-                colorscale=[[0, "rgb(166,206,227)"], [0.25, "rgb(31,120,180)"],
-                            [0.45,
-                             "rgb(178,223,138)"], [0.65, "rgb(51,160,44)"],
-                            [0.85, "rgb(251,154,153)"], [1, "rgb(227,26,28)"]]),
+                x=[i for i in range(10)]),
             layout=go.Layout(title='Market Prices')),
         style={'height': 1000},
         config={'displayModeBar': False}),
     dcc.Interval(
         id='interval-component',
-        interval=4 * 1000,  # in milliseconds
-        n_intervals=0)
+        interval=5 * 1000,  # in milliseconds
+        n_intervals=0),
 ]
 
 app.layout = html.Div(content)
@@ -53,22 +52,33 @@ app.layout = html.Div(content)
     Output('live-update-graph', 'figure'),
     [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
-    simulate(persons, houses, ask_df, bid_df)
     heatmap_data = [[
-        round(houses.iloc[i + j * 10]['last_updated'], 2) for i in range(10)
+        round(houses.iloc[i + j * 10]['market_price'], 2) for i in range(10)
     ] for j in range(10)]
     return {
         'data': [
             go.Heatmap(
                 z=heatmap_data,
                 y=[i for i in range(10)],
-                x=[i for i in range(10)],
-                colorscale=[[0, "rgb(166,206,227)"], [0.25, "rgb(31,120,180)"],
-                            [0.45,
-                             "rgb(178,223,138)"], [0.65, "rgb(51,160,44)"],
-                            [0.85, "rgb(251,154,153)"], [1, "rgb(227,26,28)"]])
+                x=[i for i in range(10)])
         ]
     }
+
+
+@app.callback(
+    Output("sim-count", "children"), [Input('start-button', 'n_clicks')])
+def update_sim_count(n):
+    global k
+    print(n)
+    mean_price = houses['market_price'].mean()
+    if (n is not None):
+        while True:
+            if (n % 2 == 0):
+                break
+            k += 1
+            simulate(persons, houses, ask_df, bid_df)
+        print('loop exited')
+        return f"No. of Sims: {k} {mean_price}"
 
 
 if __name__ == "__main__":
